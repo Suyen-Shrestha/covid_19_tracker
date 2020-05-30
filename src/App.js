31137dcb18ee057cdc6dbd4e3f09e9017a27c260
Suyen Shrestha <suyencrestha@gmail.com>
2020-05-30 15:47:13 +0545

    Added Country Selecting feature

--- src/App.js
+++ src/App.js
@@ -3,23 +3,33 @@ import {Cards, Chart, CountryPicker} from './components'
 import styles from './App.module.css'
 import {fetchData} from './api'
 
+import CoronaImg from './images/covid_logo.png'
+
 export class App extends Component {
 
     state = {
         data: {},
+        country: ''
     }
 
     async componentDidMount(){
         const fetchedData = await fetchData()
         this.setState({data: fetchedData})
     }
+
+    handleCountryChange = async (country) => {
+        const fetchedData = await fetchData(country)
+        this.setState({data: fetchedData, country: country})
+    }
+
     render() {
-        const {data} = this.state
+        const {data, country} = this.state
         return (
             <div className={styles.container}>
+                <img className={styles.image} src={CoronaImg} alt="COVID-19"/>
                 <Cards data={data}/>
-                <CountryPicker/>
-                <Chart/>
+                <CountryPicker handleCountryChange={this.handleCountryChange}/>
+                <Chart data={data} country={country}/>
             </div>
         )
     }
